1734aa5c28767cdb2718a2e86605627203d47dfc
asherk7 <masherk03@hotmail.com>
2023-07-20 18:25:17 -0400

    Created react app, deleted unnecessary files, and created file structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header/Header.js'
+import About from './components/About/About.js'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+    </div>
+  )
+}
+
+export default App
