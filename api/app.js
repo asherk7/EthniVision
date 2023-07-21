a686910538f0ec173326bd3b65ae67422e956b7f
asherk7 <masherk03@hotmail.com>
2023-07-20 22:44:20 -0400

    added module concurrently to allow for front and backend to be ran simultaniously

--- api/app.js
+++ api/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 4000;
+const routeHandler = require('./routes/handler');
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routeHandler);
 
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
