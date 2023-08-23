28ccb63ffd22496bcfff79f56f74c4bb4b84e819
asherk7 <masherk03@hotmail.com>
2023-08-23 00:33:50 -0400

    Implemented a different face detection model, updated model to encorporate transfer learning

--- ml/ethnivision_model.py
+++ ml/ethnivision_model.py
@@ -3,12 +3,14 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, BatchNormalization
+from keras.applications import VGG16
+from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, BatchNormalization, GlobalAveragePooling2D
 from tensorflow.python.keras.models import Sequential, Model
 from tensorflow.python.keras.optimizers import Adam
-from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.utils import plot_model
+from tensorflow.python.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 
-# loading preprocessed data
+# Turning the training, validation, and testing sets into dataframes
 data_dir = "C:\\Users\\mashe\\Downloads\\cropped_images"
 train_csv = "C:\\Users\\mashe\\Downloads\\csv\\train.csv"
 val_csv = "C:\\Users\\mashe\\Downloads\\csv\\val.csv"
@@ -19,8 +21,8 @@ val_df = pd.read_csv(val_csv)
 test_df = pd.read_csv(test_csv)
 
 batch_size=32
-img_size=(128, 128)
-input_shape = (128, 128, 3)
+img_size=(224, 224)
+input_shape = (224, 224, 3)
 
 # Creating data generators for data preprocessing and augmentation
 train_datagen = ImageDataGenerator(
@@ -36,6 +38,7 @@ train_datagen = ImageDataGenerator(
 
 val_datagen = ImageDataGenerator(rescale=1./255)
 
+# we use the general directory, since each dataframe has a relative path to the image from this directory
 train_generator = train_datagen.flow_from_dataframe(
     dataframe=train_df,
     directory=data_dir, 
@@ -56,25 +59,13 @@ val_generator = val_datagen.flow_from_dataframe(
     class_mode='multi_output',
 )
 
+model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
+
+for layer in model.layers:
+    layer.trainable = False
+
 # Creating our new model
 tf.random.set_seed(42)
-
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.2))
 
 # Building seperate branches for each output
 
@@ -92,6 +83,7 @@ gender_output = Dense(2, activation='sigmoid', name='gender')(gender_output)
 
 race_output = Flatten()(model.layers[-1].output)
 race_output = Dense(256, activation='relu')(race_output)
+race_output = Dense(128, activation='relu')(race_output)
 race_output = BatchNormalization()(race_output)
 race_output = Dropout(0.5)(race_output)
 race_output = Dense(7, activation='softmax', name='race')(race_output)
@@ -104,17 +96,24 @@ losses = {
     'race': 'sparse_categorical_crossentropy'
 }
 
-learning_rate = 1e-5
-epochs = 100
+loss_weights = {
+    'age': 2,
+    'gender': 0.2,
+    'race': 3.5
+}
+
+learning_rate = 1e-4
+epochs = 30
 
 ethnivision_model.compile(
     loss=losses,
+    loss_weights=loss_weights,
     optimizer=Adam(learning_rate=learning_rate),
     metrics=['accuracy']
 )
 
 callbacks = [
-    ModelCheckpoint("/model_checkpoint", monitor='val_loss', save_best_only=True)
+    ModelCheckpoint("./model_checkpoint", monitor='val_loss', save_best_only=True)
 ]
 
 history = ethnivision_model.fit(
